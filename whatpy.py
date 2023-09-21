7836fae77bd1cd1ff8879f43a50ad57e0adfc983
saba-shamshad <145676919+saba-shamshad@users.noreply.github.com>
2023-09-21 18:30:59 +0530

    Add files via upload

---
+++ whatpy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pip install pywhatkit
+
+
+# In[ ]:
+
+
+import pywhatkit
+pywhatkit.sendwhatmsg("number", "good after noon sir!!have a nice day", time(hrs),time(min))
+
